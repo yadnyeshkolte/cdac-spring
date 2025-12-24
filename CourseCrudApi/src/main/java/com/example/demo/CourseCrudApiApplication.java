f1b540648b863c202e82dbded96bf2de552adeef
Yadnyesh <yadnyeshkolte@gmail.com>
2025-12-24 14:28:15 +0530

    update

---
+++ CourseCrudApi/src/main/java/com/example/demo/CourseCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseCrudApiApplication.class, args);
+	}
+
+}
